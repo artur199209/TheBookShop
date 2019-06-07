970488145e948150345c2a615f789012767b04b6
artur199209 <arti190@gmail.com>
2019-06-07 11:36:24 +0200

    Create ProductsListViewModel.cs

---
+++ TheBookShop/Infrastructure/ProductsListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TheBookShop.Models;
+using TheBookShop.Models.ViewModel;
+
+namespace TheBookShop.Infrastructure
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
