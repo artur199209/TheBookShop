2990b1d292621031560b9ac55265bc7f579c3445
artur199209 <arti190@gmail.com>
2019-06-07 12:22:37 +0200

    Add NavigationMenu component

---
+++ TheBookShop/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBookShop.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models;
+
+namespace TheBookShop.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repository;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x));
+        }
+    }
+}
