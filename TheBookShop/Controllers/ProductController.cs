bc1e97a537d3f859040856578454b7e60fbfc194
artur199209 <arti190@gmail.com>
2019-06-07 11:35:41 +0200

    Update ProductController.cs

--- TheBookShop/Controllers/ProductController.cs
+++ TheBookShop/Controllers/ProductController.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Infrastructure;
 using TheBookShop.Models;
+using TheBookShop.Models.ViewModel;
 
 namespace TheBookShop.Controllers
 {
     public class ProductController : Controller
     {
         private IProductRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IProductRepository repo)
         {
             repository = repo;
         }
 
-        public ViewResult List() => View(repository.Products);
+        public ViewResult List(string category, int productPage = 1)
+            => View(new ProductsListViewModel
+            {
+                Products = repository.Products
+                    .Where(p => category == null || p.Category == category)
+                    .OrderBy(p => p.ProductId)
+                    .Skip((productPage - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = productPage,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                },
+                CurrentCategory = category
+            });
     }
 }
