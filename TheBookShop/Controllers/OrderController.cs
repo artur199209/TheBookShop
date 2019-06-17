3a4d1b62787a0d4b57bb827a3a83d174da051cad
artur199209 <arti190@gmail.com>
2019-06-17 13:30:30 +0200

    Create OrderController.cs

---
+++ TheBookShop/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models;
+
+namespace TheBookShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderRepository repository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository repoService, Cart cartService)
+        {
+            repository = repoService;
+            cart = cartService;
+        }
+
+        public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Twój koszyk jest pusty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                return RedirectToAction(nameof(Completed));
+            }
+            
+             return View(order);
+        }
+
+        public ViewResult Completed()
+        {
+            cart.Clear();
+            return View();
+        }
+    }
+}
