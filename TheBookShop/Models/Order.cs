903e6eb15d3acbe11bb4dec7a5d0150d29968b0b
artur199209 <arti190@gmail.com>
2019-06-17 13:29:38 +0200

    Create Order.cs

---
+++ TheBookShop/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace TheBookShop.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Country { get; set; }
+        public bool GiftWrap { get; set; }
+    }
+}
