577698f6da1bf3b01ee6b432e984b69538a7ce39
artur199209 <arti190@gmail.com>
2019-07-04 09:54:25 +0200

    Update IProductRepository.cs

--- TheBookShop/Models/IProductRepository.cs
+++ TheBookShop/Models/IProductRepository.cs
@@ -5,5 +5,9 @@ namespace TheBookShop.Models
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
+
+        void SaveProduct(Product product);
+
+        Product DeleteProduct(int productId);
     }
 }
