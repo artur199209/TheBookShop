cb790d7d86f29fbdbd7ff61d0126cd1c6a79ee4f
artur199209 <arti190@gmail.com>
2019-07-04 09:55:31 +0200

    Add SaveProduct and DeleteProduct methods

--- TheBookShop/Models/EFProductRepository.cs
+++ TheBookShop/Models/EFProductRepository.cs
@@ -15,5 +15,39 @@ namespace TheBookShop.Models
         }
 
         public IQueryable<Product> Products => context.Products;
+        public void SaveProduct(Product product)
+        {
+            if (product.ProductId == 0)
+            {
+                context.Products.Add(product);
+            }
+            else
+            {
+                Product productEntry = context.Products.FirstOrDefault(p => p.ProductId == product.ProductId);
+
+                if (productEntry != null)
+                {
+                    productEntry.Name = product.Name;
+                    productEntry.Category = product.Category;
+                    productEntry.Description = product.Description;
+                    productEntry.Price = product.Price;
+                }
+            }
+
+            context.SaveChanges();
+        }
+
+        public Product DeleteProduct(int productId)
+        {
+            Product productEntry = context.Products.FirstOrDefault(p => p.ProductId == productId);
+
+            if (productEntry != null)
+            {
+                context.Products.Remove(productEntry);
+                context.SaveChanges();
+            }
+
+            return productEntry;
+        }
     }
 }
