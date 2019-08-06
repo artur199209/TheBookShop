c19fb5789df31a56b340b1b1ed8ddc05fdee1350
artur199209 <arti190@gmail.com>
2019-08-06 12:57:44 +0200

    Create TopNavigationViewComponent.cs

---
+++ TheBookShop/Components/TopNavigationViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBookShop.Components
+{
+    public class TopNavigationViewComponent : ViewComponent
+    {
+        private readonly string[] _names = {"Top 100", "Nowości", "Zapowiedzi", "Nauka", "Przedsprzedaż"};
+        
+        public IViewComponentResult Invoke()
+        {
+            return View(_names);
+        }
+    }
+}
