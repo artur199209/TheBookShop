4e56665045505a6723a91274b38f4043b2566539
artur199209 <arti190@gmail.com>
2019-09-03 15:41:05 +0200

    Create Opinion.cs

---
+++ TheBookShop/Models/Opinion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TheBookShop.Models
+{
+    public class Opinion
+    {
+        public int OpinionId { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public string OpinionDescription { get; set; }
+        public DateTime OpinionDate { get; set; }
+    }
+}
