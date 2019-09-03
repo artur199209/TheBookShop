61e702c503166401f9077d7b7f9cef3ab58237cb
artur199209 <arti190@gmail.com>
2019-09-03 13:42:50 +0200

    Create Customer.cs

---
+++ TheBookShop/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace TheBookShop.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public string City { get; set; }
+        public string Street { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
