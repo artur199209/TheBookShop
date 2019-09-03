45198ae5dc3ae70dd35a1a48f6a03d2b3a33a840
artur199209 <arti190@gmail.com>
2019-09-03 13:44:38 +0200

    Add Carousel ViewComponent

---
+++ TheBookShop/Components/CarouselViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Infrastructure;
+using TheBookShop.Models;
+
+namespace TheBookShop.Components
+{
+    public class CarouselViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CarouselViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke(string category, int id)
+        {
+            var p = new CarouselViewModel();
+            p.Category = category;
+            p.Id = id;
+            return View(p);
+        }
+    }
+}
