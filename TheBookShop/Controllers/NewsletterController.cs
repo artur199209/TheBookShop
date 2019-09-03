6f6d0482d38794c40a6ccd259c1eb82337db9db8
artur199209 <arti190@gmail.com>
2019-09-03 13:45:37 +0200

    Create NewsletterController.cs

---
+++ TheBookShop/Controllers/NewsletterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBookShop.Controllers
+{
+    public class NewsletterController : Controller
+    {
+        public IActionResult Subscribe()
+        {
+            return View();
+        }
+    }
+}
