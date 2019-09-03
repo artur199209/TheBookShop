71158e58b83859b645ceb96233d9819702544f02
artur199209 <arti190@gmail.com>
2019-09-03 13:45:45 +0200

    Create SearchController.cs

---
+++ TheBookShop/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBookShop.Controllers
+{
+    public class SearchController : Controller
+    {
+        public IActionResult Search()
+        {
+            return View();
+        }
+    }
+}
