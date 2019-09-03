904b4fea94485d4456016c8bb5d79d8918dcda79
artur199209 <arti190@gmail.com>
2019-09-03 15:41:20 +0200

    Update Customer.cs

--- TheBookShop/Models/Customer.cs
+++ TheBookShop/Models/Customer.cs
@@ -12,8 +12,12 @@
 
         public string Email { get; set; }
 
+        public string Country { get; set; }
+
         public string City { get; set; }
+
         public string Street { get; set; }
+
         public string ZipCode { get; set; }
     }
 }
