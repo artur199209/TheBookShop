ff710e0e02c7e5978f36227a43e5b3de3ab3be74
artur199209 <arti190@gmail.com>
2019-09-03 13:44:46 +0200

    Create CarouselViewModel.cs

---
+++ TheBookShop/Infrastructure/CarouselViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TheBookShop.Models;
+
+namespace TheBookShop.Infrastructure
+{
+    public class CarouselViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public string Category { get; set; }
+        public int Id { get; set; }
+    }
+}
