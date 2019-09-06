dce2fa8dd84c231cb6af8cbaffc922ce5b2dbe85
artur199209 <arti190@gmail.com>
2019-09-06 12:20:28 +0200

    Add FileHelper class

---
+++ TheBookShop/Infrastructure/FileHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace TheBookShop.Infrastructure
+{
+    public static class FileHelper
+    {
+        private static string WebRootPath { get;} = Path.Combine(Environment.CurrentDirectory, "wwwroot");
+
+        public static void CopyImageFile(IFormFile file)
+        {
+            var uploads = Path.Combine(WebRootPath, "Images\\");
+            if (file.Length > 0)
+            {
+                var fileName = Guid.NewGuid().ToString().Replace("-", "") + Path.GetExtension(file.FileName);
+                using (var fileStream = new FileStream(Path.Combine(uploads, fileName), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                   
+                }
+
+            }
+        }
+    }
+}
