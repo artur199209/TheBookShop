3679085101ab8ff49f45c56c386b500d3a5e0670
artur199209 <arti190@gmail.com>
2019-09-09 12:51:48 +0200

    Create CustomerAddress.cs

---
+++ TheBookShop/Models/CustomerAddress.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace TheBookShop.Models
+{
+    public class CustomerAddress
+    {
+        [BindNever]
+        public int CustomerAddressId { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+        public string HomeNumber { get; set; }
+    }
+}
