8bc524f5bd7937cff0beeadba487f663567e8259
artur199209 <arti190@gmail.com>
2019-09-09 15:05:17 +0200

    Create EFAuthorRepository.cs

---
+++ TheBookShop/Models/EFAuthorRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace TheBookShop.Models
+{
+    public class EFAuthorRepository : IAuthorRepository
+    {
+        private ApplicationDbContext _applicationDbContext;
+
+        public EFAuthorRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public IQueryable<Author> Authors => _applicationDbContext.Authors;
+
+        public void SaveAuthor(Author author)
+        {
+            try
+            {
+                if (author.AuthorId == 0)
+                {
+                    _applicationDbContext.Authors.Add(author);
+                }
+                else
+                {
+                    var authorEntry = _applicationDbContext.Authors.FirstOrDefault(x => x.AuthorId == author.AuthorId);
+
+                    if (authorEntry != null)
+                    {
+                        authorEntry.Name = author.Name;
+                        authorEntry.Surname = author.Surname;
+                        authorEntry.Notes = author.Notes;
+                    }
+                }
+
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
