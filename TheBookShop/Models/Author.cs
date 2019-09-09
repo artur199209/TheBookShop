2679c88b640e0dc81b78e671d02b336f69a73391
artur199209 <arti190@gmail.com>
2019-09-09 12:51:41 +0200

    Create Author.cs

---
+++ TheBookShop/Models/Author.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TheBookShop.Models
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Notes { get; set; }
+    }
+}
