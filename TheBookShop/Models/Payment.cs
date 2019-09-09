9bcae11dcf99929e0b234b9765d989f4f6b76c85
artur199209 <arti190@gmail.com>
2019-09-09 12:52:10 +0200

    Update Payment.cs

--- TheBookShop/Models/Payment.cs
+++ TheBookShop/Models/Payment.cs
@@ -5,7 +5,8 @@ namespace TheBookShop.Models
     public class Payment
     {
         public int CustomerId { get; set; }
-        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public Customer Customer { get; set; }
         public DateTime PaymentDate { get; set; }
         public decimal Amount { get; set; }
     }
