722ff6f390dc8ce8ba25b564de52d57bf549f611
artur199209 <arti190@gmail.com>
2019-09-09 12:51:57 +0200

    Update Opinion.cs

--- TheBookShop/Models/Opinion.cs
+++ TheBookShop/Models/Opinion.cs
@@ -5,8 +5,8 @@ namespace TheBookShop.Models
     public class Opinion
     {
         public int OpinionId { get; set; }
-        public int ProductId { get; set; }
-        public int CustomerId { get; set; }
+        public Product Product { get; set; }
+        public Customer Customer { get; set; }
         public string OpinionDescription { get; set; }
         public DateTime OpinionDate { get; set; }
     }
