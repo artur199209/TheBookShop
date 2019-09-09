584d4adc6c9c2e044bdd191c508042038bd432c0
artur199209 <arti190@gmail.com>
2019-09-09 15:05:10 +0200

    Update ApplicationDbContext.cs

--- TheBookShop/Models/ApplicationDbContext.cs
+++ TheBookShop/Models/ApplicationDbContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace TheBookShop.Models
 {
@@ -13,5 +9,6 @@ namespace TheBookShop.Models
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<Author> Authors { get; set; }
     }
 }
