2cb17b0b0d159c1402e858e778d2115d09e18c69
artur199209 <arti190@gmail.com>
2019-09-10 08:09:56 +0200

    Add DeliveryAddressRepository

---
+++ TheBookShop/Models/EFDeliveryAddressRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace TheBookShop.Models
+{
+    public class EFDeliveryAddressRepository : IDeliveryAdressRepository
+    {
+        private ApplicationDbContext applicationDbContext;
+
+        public EFDeliveryAddressRepository(ApplicationDbContext _applicationDbContext)
+        {
+            applicationDbContext = _applicationDbContext;
+        }
+
+        public IQueryable<DeliveryAddress> DeliveryAddresses => applicationDbContext.DeliveryAddresses;
+
+        public void SaveDeliveryAddress(DeliveryAddress deliveryAddress)
+        {
+            try
+            {
+                applicationDbContext.DeliveryAddresses.Add(deliveryAddress);
+                applicationDbContext.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
