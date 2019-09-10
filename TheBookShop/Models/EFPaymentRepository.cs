d69c64a34451c28fbf21c7c7622921e7eac9daa5
artur199209 <arti190@gmail.com>
2019-09-10 08:10:16 +0200

    Add Payment Repository

---
+++ TheBookShop/Models/EFPaymentRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheBookShop.Models
+{
+    public class EFPaymentRepository : IPaymentRepository
+    {
+        private ApplicationDbContext applicationDbContext;
+
+        public EFPaymentRepository(ApplicationDbContext _applicationDbContext)
+        {
+            applicationDbContext = _applicationDbContext;
+        }
+
+        public IQueryable<Payment> Payments => applicationDbContext.Payments;
+
+        public void SavePayment(Payment payment)
+        {
+            try
+            {
+                if (payment.PaymentId == 0)
+                {
+                    applicationDbContext.Payments.Add(payment);
+                }
+                else
+                {
+                    var paymentEntry = applicationDbContext.Payments
+                        .FirstOrDefault(x => x.PaymentId == payment.PaymentId);
+
+                    if (paymentEntry != null)
+                    {
+                        paymentEntry.Amount = payment.Amount;
+                        paymentEntry.Customer = payment.Customer;
+                        paymentEntry.PaymentDate = payment.PaymentDate;
+                    }
+                }
+
+                applicationDbContext.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
