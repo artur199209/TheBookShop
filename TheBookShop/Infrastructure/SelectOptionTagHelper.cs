1a7f750ea25246f0696efc37af860fcd2667211a
artur199209 <arti190@gmail.com>
2019-09-10 13:33:26 +0200

    Create select option tag helper for displaying authors

---
+++ TheBookShop/Infrastructure/SelectOptionTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+using TheBookShop.Models;
+
+namespace TheBookShop.Infrastructure
+{
+    [HtmlTargetElement("select", Attributes = "model-for")]
+    public class SelectOptionTagHelper : TagHelper
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public SelectOptionTagHelper(IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public ModelExpression ModelFor { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml((await output.GetChildContentAsync(false)).GetContent());
+
+            string selected = ModelFor.Model as string;
+
+            foreach (Author author in _authorRepository.Authors)
+            {
+                output.Content.AppendHtml(selected != null
+                    ? $"<option selected value={author.AuthorId}>{author.Name} {author.Surname} </option>"
+                    : $"<option value={author.AuthorId}>{author.Name} {author.Surname} </option>");
+            }
+
+            output.Attributes.SetAttribute("Name", ModelFor.Name);
+            output.Attributes.SetAttribute("Id", ModelFor.Name);
+        }
+    }
+}
