5057ebe1c1d7b703374ae80da49292e8dc238776
artur199209 <arti190@gmail.com>
2019-09-12 11:40:35 +0200

    Update AuthorController.cs

--- TheBookShop/Areas/Admin/Controllers/AuthorController.cs
+++ TheBookShop/Areas/Admin/Controllers/AuthorController.cs
@@ -1,19 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using TheBookShop.Models;
 
-namespace TheBookShop.Controllers
+namespace TheBookShop.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [Route("Admin/[controller]")]
     public class AuthorController : Controller
     {
-        private IAuthorRepository _authorRepository;
+        private readonly IAuthorRepository _authorRepository;
 
         public AuthorController(IAuthorRepository authorRepository)
         {
             _authorRepository = authorRepository;
         }
         
+        [Route("")]
         [Route("[action]")]
         public IActionResult Index()
         {
@@ -23,11 +25,19 @@ namespace TheBookShop.Controllers
         [Route("[action]")]
         public IActionResult Create()
         {
-            return View();
+            return View(nameof(Edit), new Author());
+        }
+
+        [Route("[action]")]
+        public IActionResult Edit(int authorId)
+        {
+            var author = _authorRepository.Authors.FirstOrDefault(x => x.AuthorId == authorId);
+            return View(author);
         }
 
         [HttpPost]
-        public IActionResult Create(Author author)
+        [Route("[action]")]
+        public IActionResult Edit(Author author)
         {
             if (ModelState.IsValid)
             {
@@ -37,6 +47,26 @@ namespace TheBookShop.Controllers
             }
 
             return View(author);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public IActionResult Delete(int authorId)
+        {
+            var deletedAuthor = _authorRepository.DeleteAuthor(authorId);
+
+            if (deletedAuthor != null)
+            {
+                TempData["message"] = $"{deletedAuthor.Name} {deletedAuthor.Surname } was deleted";
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        [Route("[action]")]
+        public IActionResult Books(int authorId)
+        {
+            var authorWithBooks = _authorRepository.GetAuthorById(authorId);
+            return View(authorWithBooks);
         }
     }
 }
