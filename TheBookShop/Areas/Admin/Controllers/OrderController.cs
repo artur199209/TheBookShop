27fca97a45e919d0e854272de7033132a0fd0599
artur199209 <arti190@gmail.com>
2019-09-30 15:23:18 +0200

    Create Order controller with index view

---
+++ TheBookShop/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices.WindowsRuntime;
+using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models;
+
+namespace TheBookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[controller]")]
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository _orderRepository;
+
+        public OrderController(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View(_orderRepository.Orders);
+        }
+    }
+}
