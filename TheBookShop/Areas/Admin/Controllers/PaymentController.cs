128f50fd656ff325a11280b39fe48b6dc75e344c
artur199209 <arti190@gmail.com>
2019-10-15 14:23:45 +0200

    Create PaymentController.cs

---
+++ TheBookShop/Areas/Admin/Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models;
+
+namespace TheBookShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/[controller]")]
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentRepository _paymentRepository;
+
+        public PaymentController(IPaymentRepository paymentRepository)
+        {
+            _paymentRepository = paymentRepository;
+        }
+
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View(_paymentRepository.Payments);
+        }
+    }
+}
