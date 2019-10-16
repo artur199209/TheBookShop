c657c73ffc22f353356831a5f8a4251d9c12f89b
artur199209 <arti190@gmail.com>
2019-10-16 14:10:10 +0200

    Create AuthorListViewModel.cs

---
+++ TheBookShop/Areas/Admin/Model/AuthorListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TheBookShop.Models;
+using TheBookShop.Models.ViewModel;
+
+namespace TheBookShop.Areas.Admin.Model
+{
+    public class AuthorListViewModel
+    {
+        public IEnumerable<Author> Authors { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
