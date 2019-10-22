ef6a2a9ae4cedf67d9ce9d9764f9117243a1f03c
artur199209 <arti190@gmail.com>
2019-10-22 15:43:35 +0200

    Implement edit method in role controllers

--- TheBookShop/Areas/Admin/Controllers/RoleController.cs
+++ TheBookShop/Areas/Admin/Controllers/RoleController.cs
@@ -1,7 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Areas.Admin.Model;
 using TheBookShop.Models;
 
 namespace TheBookShop.Areas.Admin.Controllers
@@ -56,7 +58,69 @@ namespace TheBookShop.Areas.Admin.Controllers
         [Route("[action]")]
         public async Task<IActionResult> Edit(string id)
         {
-            return View();
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+
+            var members = new List<AppUser>();
+            var nonMembers = new List<AppUser>();
+
+            foreach (var user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+            
+            return View(new RoleEditModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Edit(RoleModificationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result;
+                foreach (string userId in model.IdsToAdd ?? new string [] {})
+                {
+                    AppUser user = await _userManager.FindByIdAsync(userId);
+
+                    if (user != null)
+                    {
+                        result = await _userManager.AddToRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            AddErrosFromResult(result);
+                        }
+                    }
+                }
+
+                foreach (string userId in model.IdsToDelete ?? new string[] {})
+                {
+                    AppUser user = await _userManager.FindByIdAsync(userId);
+
+                    if (user != null)
+                    {
+                        result = await _userManager.RemoveFromRoleAsync(user, model.RoleName);
+
+                        if (!result.Succeeded)
+                        {
+                            AddErrosFromResult(result);
+                        }
+                    }
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            return await Edit(model.RoleId);
         }
 
         [HttpPost]
