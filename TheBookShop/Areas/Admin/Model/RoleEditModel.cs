81b8f35bf0b80fd7b8398d2099a231229f1d7c41
artur199209 <arti190@gmail.com>
2019-10-22 15:43:22 +0200

    Add models for roles

---
+++ TheBookShop/Areas/Admin/Model/RoleEditModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using TheBookShop.Models;
+
+namespace TheBookShop.Areas.Admin.Model
+{
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<AppUser> Members { get; set; }
+        public IEnumerable<AppUser> NonMembers { get; set; }
+    }
+}
---
+++ TheBookShop/Areas/Admin/Model/RoleModificationModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheBookShop.Areas.Admin.Model
+{
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
