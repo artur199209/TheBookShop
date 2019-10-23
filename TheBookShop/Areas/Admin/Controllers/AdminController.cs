039da978bb8b473e4f238064e67eadcd5f68d96d
artur199209 <arti190@gmail.com>
2019-10-23 15:27:12 +0200

    Add Login, Logout, AccessDenied methods

--- TheBookShop/Areas/Admin/Controllers/AdminController.cs
+++ TheBookShop/Areas/Admin/Controllers/AdminController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
 
 namespace TheBookShop.Areas.Admin.Controllers
 {
@@ -6,6 +11,15 @@ namespace TheBookShop.Areas.Admin.Controllers
     [Route("[controller]")]
     public class AdminController : Controller
     {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public AdminController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [Route("[action]")]
         public IActionResult Index()
         {
@@ -14,7 +28,49 @@ namespace TheBookShop.Areas.Admin.Controllers
 
         [Route("")]
         [Route("[action]")]
+        [AllowAnonymous]
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [Route("")]
+        public async Task<IActionResult> Login(LoginModel details)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(details.Email);
+
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+
+                    SignInResult result =
+                        await _signInManager.PasswordSignInAsync(user, details.Password, false, false);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginModel.Email), "Niepoprawny adres email albo hasło!");
+            }
+
+            return View(details);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Index));
+        }
+     
+        [AllowAnonymous]
+        [Route("[action]")]
+        public IActionResult AccessDenied()
         {
             return View();
         }
