edd960ced67ca6cc2130e511f7b409121998fb8c
artur199209 <arti190@gmail.com>
2019-10-28 12:57:38 +0100

    Create AdminUser view component

---
+++ TheBookShop/Areas/Admin/Components/AdminUserViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheBookShop.Areas.Admin.Components
+{
+    public class AdminUserViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
