99d29e462c66e897bf8b2b252df49f69cc29f24c
artur199209 <arti190@gmail.com>
2019-11-08 13:44:53 +0100

    Add Login function

---
+++ TheBookShop/Controllers/UserController.cs
@@ -0,0 +1,66 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models.DataModels;
+using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
+
+namespace TheBookShop.Controllers
+{
+    [Route("[controller]")]
+    public class UserController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [Route("")]
+        [Route("[action]")]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        [Route("[action]")]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if (user != null)
+                {
+                    await _signInManager.SignOutAsync();
+
+                    SignInResult result =
+                        await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
+                    
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Product");
+                    }
+                }
+
+                ModelState.AddModelError(nameof(LoginModel.Email), "Niepoprawny adres email albo hasło!");
+            }
+
+            return View(model);
+        }
+
+        [Route("[action]")]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
