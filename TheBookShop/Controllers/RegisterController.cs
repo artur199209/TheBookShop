96e8334e4d20f753cee23012e066c11ed7555cf5
artur199209 <arti190@gmail.com>
2019-11-08 13:44:37 +0100

    Add Register function

---
+++ TheBookShop/Controllers/RegisterController.cs
@@ -0,0 +1,58 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models.DataModels;
+
+namespace TheBookShop.Controllers
+{
+    [Route("[controller]")]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [Route("")]
+        [Route("[action]")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        [Route("[action]")]
+        public async Task<IActionResult> Register(CreateModel model)
+        {
+            AppUser user = new AppUser
+            {
+                UserName = model.Name,
+                Email = model.Email
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("index", "Product", new {area = ""});
+            }
+
+            AddErrorsFromResult(result);
+
+            return View(model);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+        }
+    }
+}
