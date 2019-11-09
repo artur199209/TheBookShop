338e741adbd5b88584cc46d1da622671ca5b4b07
artur199209 <arti190@gmail.com>
2019-11-09 16:21:48 +0100

    check if passwords are the same during register

--- TheBookShop/Models/DataModels/CreateModel.cs
+++ TheBookShop/Models/DataModels/CreateModel.cs
@@ -4,14 +4,21 @@ namespace TheBookShop.Models.DataModels
 {
     public class CreateModel
     {
-        [Required]
+        [Required(ErrorMessage = "Proszę podać nazwę użytkownika.")]
         [Display(Name = "Nazwa")]
         public string Name { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Proszę podać adres email.")]
         [Display(Name = "Email")]
         public string Email { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Proszę podać hasło.")]
         [Display(Name = "Hasło")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać hasło jeszcze raz.")]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare(nameof(Password), ErrorMessage = "Podane hasła różnią się!")]
+        public string ConfirmPassword { get; set; }
     }
 }
