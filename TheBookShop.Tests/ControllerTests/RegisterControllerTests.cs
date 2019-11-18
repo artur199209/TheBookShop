b13227f5d19a6a1b806d4a709e353cde7f02ee74
artur199209 <arti190@gmail.com>
2019-11-18 16:12:16 +0100

    Add tests for RegisterController

---
+++ TheBookShop.Tests/ControllerTests/RegisterControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using TheBookShop.Controllers;
+using TheBookShop.Models.DataModels;
+using Xunit;
+
+namespace TheBookShop.Tests.ControllerTests
+{
+    public class RegisterControllerTests
+    {
+        private readonly Mock<UserManager<AppUser>> _userManagerMock;
+
+        public RegisterControllerTests()
+        {
+            var userStoreMock = new Mock<IUserStore<AppUser>>();
+            _userManagerMock = new Mock<UserManager<AppUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+        }
+
+        [Fact]
+        public void Can_Create_New_User()
+        {
+            _userManagerMock.Setup(x => x.CreateAsync(It.IsAny<AppUser>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+            var registerController = new RegisterController(_userManagerMock.Object);
+
+            var result = registerController.Register(new CreateModel()).Result;
+            _userManagerMock.Verify(m => m.CreateAsync(It.IsAny<AppUser>(), It.IsAny<string>()));
+            Assert.Equal("Index", GetActionName(result));
+        }
+
+        [Fact]
+        public void Cannot_Create_User_When_Model_Is_Invalid()
+        {
+            var registerController = new RegisterController(_userManagerMock.Object);
+
+            registerController.ModelState.AddModelError("error", "error");
+            var result = registerController.Register(null).Result;
+            _userManagerMock.Verify(x => x.CreateAsync(It.IsAny<AppUser>()), Times.Never());
+            Assert.IsType<ViewResult>(result);
+        }
+
+        private string GetActionName(IActionResult result)
+        {
+            return (result as RedirectToActionResult)?.ActionName;
+        }
+    }
+}
