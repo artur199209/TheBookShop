b299d400c8c59d22e0f73ded80fd9447618eddaa
artur199209 <arti190@gmail.com>
2019-11-22 13:21:06 +0100

    Add new tests

--- TheBookShop.Tests/AdminTests/ControllerTests/OrderControllerTests.cs
+++ TheBookShop.Tests/AdminTests/ControllerTests/OrderControllerTests.cs
@@ -18,11 +18,11 @@ namespace TheBookShop.Tests.AdminTests.ControllerTests
             _orderRepositoryMock = new Mock<IOrderRepository>();
             _orderRepositoryMock.Setup(x => x.Orders).Returns(new[]
             {
-                new Order { Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = true },
-                new Order { Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = false },
-                new Order { Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = false },
-                new Order { Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = true },
-                new Order { Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = true }
+                new Order { OrderId = 1, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = true },
+                new Order { OrderId = 2, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = false },
+                new Order { OrderId = 3, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = false },
+                new Order { OrderId = 4, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = true },
+                new Order { OrderId = 5, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Shipped = true }
             }.AsQueryable());
         }
 
@@ -54,6 +54,30 @@ namespace TheBookShop.Tests.AdminTests.ControllerTests
 
             Assert.NotNull(result.Where(x => x.Shipped));
             Assert.Equal(2, result.Length);
+        }
+
+        [Fact]
+        public void Can_Display_Existing_Order()
+        {
+            var orderController = new OrderController(_orderRepositoryMock.Object);
+
+            var order1 = GetViewModel<Order>(orderController.Details(1));
+            var order2 = GetViewModel<Order>(orderController.Details(2));
+            var order3 = GetViewModel<Order>(orderController.Details(3));
+
+            Assert.Equal(1, order1?.OrderId);
+            Assert.Equal(2, order2?.OrderId);
+            Assert.Equal(3, order3?.OrderId);
+        }
+
+        [Fact]
+        public void Cannot_Display_Non_Existing_Order()
+        {
+            var orderController = new OrderController(_orderRepositoryMock.Object);
+
+            var order = GetViewModel<Order>(orderController.Details(99));
+
+            Assert.Null(order);
         }
 
         private T GetViewModel<T>(IActionResult result) where T : class
