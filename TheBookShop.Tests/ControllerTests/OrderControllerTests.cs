20e5c63691feaf902581e82baa69e86913c7dd31
artur199209 <arti190@gmail.com>
2019-11-22 13:06:22 +0100

    update tests

--- TheBookShop.Tests/ControllerTests/OrderControllerTests.cs
+++ TheBookShop.Tests/ControllerTests/OrderControllerTests.cs
@@ -31,6 +31,7 @@ namespace TheBookShop.Tests.ControllerTests
 
             _orderRepositoryMock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
             Assert.True(string.IsNullOrEmpty(result?.ViewName));
+            Assert.False(target.ModelState.IsValid);
         }
 
         [Fact]
@@ -45,6 +46,7 @@ namespace TheBookShop.Tests.ControllerTests
 
             _orderRepositoryMock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
             Assert.True(string.IsNullOrEmpty(result?.ViewName));
+            Assert.False(target.ModelState.IsValid);
         }
 
         [Fact]
