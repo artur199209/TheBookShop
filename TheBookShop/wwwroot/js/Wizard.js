cd11deb7d7c62a8dd0898c315a5b787289dd9cba
artur199209 <arti190@gmail.com>
2019-11-22 11:00:07 +0100

    update wizard css and jsa

--- TheBookShop/wwwroot/js/Wizard.js
+++ TheBookShop/wwwroot/js/Wizard.js
@@ -26,31 +26,7 @@
             duration: 600
         });
     });
-
-    $(".next2").click(function () {
-
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next().first();
-        //Add Class Active
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({ opacity: 0 }, {
-            step: function (now) {
-                // for making fielset appear animation
-                opacity = 1 - now;
-
-                current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
-                });
-                next_fs.css({ 'opacity': opacity });
-            },
-            duration: 600
-        });
-    });
-
+    
     $(".previous").click(function () {
 
         current_fs = $(this).parent();
