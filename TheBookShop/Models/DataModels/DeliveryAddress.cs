784b5957fa3e3858c1373126f5442dae30131738
artur199209 <arti190@gmail.com>
2019-11-26 15:01:23 +0100

    Update DeliveryAddress model

--- TheBookShop/Models/DataModels/DeliveryAddress.cs
+++ TheBookShop/Models/DataModels/DeliveryAddress.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace TheBookShop.Models.DataModels
 {
@@ -6,9 +7,20 @@ namespace TheBookShop.Models.DataModels
     {
         [BindNever]
         public int DeliveryAddressId { get; set; }
+        [Required(ErrorMessage = "Proszę podać kraj.")]
+        [Display(Name = "Kraj")]
         public string Country { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać miasto.")]
+        [Display(Name = "Miasto")]
         public string City { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać kod pocztowy.")]
+        [Display(Name = "Kod pocztowy")]
         public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać numer domu/mieszkania.")]
+        [Display(Name = "Nr domu/mieszkania")]
         public string HomeNumber { get; set; }
     }
 }
