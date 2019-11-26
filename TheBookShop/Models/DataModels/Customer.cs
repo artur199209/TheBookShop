d369aa313a0232824e7089a39640d94db8d79c6c
artur199209 <arti190@gmail.com>
2019-11-26 14:49:04 +0100

    Update Customer model

--- TheBookShop/Models/DataModels/Customer.cs
+++ TheBookShop/Models/DataModels/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TheBookShop.Models.DataModels
 {
@@ -6,12 +7,20 @@ namespace TheBookShop.Models.DataModels
     {
         public int CustomerId { get; set; }
 
+        [Required(ErrorMessage = "Proszę podać imię.")]
+        [Display(Name = "Imię")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Proszę podać nazwisko.")]
+        [Display(Name = "Nazwisko")]
         public string Surname { get; set; }
 
+        [Required(ErrorMessage = "Proszę podać numer telefonu.")]
+        [Display(Name = "Numer telefonu")]
         public string PhoneNumber { get; set; }
 
+        [Required(ErrorMessage = "Proszę podać adres email.")]
+        [Display(Name = "Email")]
         public string Email { get; set; }
 
         public CustomerAddress CustomerAddress { get; set; }
