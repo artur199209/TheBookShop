a94055f71b86ede9d169bde5b9d60b97899f0571
artur199209 <arti190@gmail.com>
2019-11-26 14:50:56 +0100

    Refactor Wizard js

--- TheBookShop/wwwroot/js/Wizard.js
+++ TheBookShop/wwwroot/js/Wizard.js
@@ -1,54 +1,67 @@
-﻿$(document).ready(function () {
-
-    var current_fs, next_fs, previous_fs; //fieldsets
+﻿$(document).ready(function ()
+{
+    var currentFs, nextFs, previousFs;
     var opacity;
 
     $(".next").click(function () {
+        str1 = "next1";
+        str2 = "next2";
 
-        current_fs = $(this).parent();
-        next_fs = $(this).parent().next();
-        //Add Class Active
-        $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-        //show the next fieldset
-        next_fs.show();
-        //hide the current fieldset with style
-        current_fs.animate({ opacity: 0 }, {
-            step: function (now) {
-                // for making fielset appear animation
-                opacity = 1 - now;
+        if (!str1.localeCompare($(this).attr('id')) && validatePersonData() === true) {
+            val1 = true;
+        } else {
+            val1 = false;
+        }
 
-                current_fs.css({
-                    'display': 'none',
-                    'position': 'relative'
+        if (!str2.localeCompare($(this).attr('id')) && validate2(0) === true) {
+            val2 = true;
+        }
+        else {
+            val2 = false;
+        }
+
+        if ((!str1.localeCompare($(this).attr('id')) && val1 === true) || (!str2.localeCompare($(this).attr('id')) && val2 === true)) {
+            currentFs = $(this).parent();
+            nextFs = $(this).parent().next();
+            var nextFs2 = $(this).parent().next().next();
+           
+            $("#progressbar li").eq($("fieldset").index(nextFs2)).addClass("active");
+            nextFs.show();
+            currentFs.animate({ opacity: 0 },
+                {
+                    step: function (now) {
+                        opacity = 1 - now;
+
+                        currentFs.css({
+                            'display': 'none',
+                            'position': 'relative'
+                        });
+                        nextFs.css({ 'opacity': opacity });
+                    },
+                    duration: 600
                 });
-                next_fs.css({ 'opacity': opacity });
-            },
-            duration: 600
-        });
+        }
     });
     
-    $(".previous").click(function () {
-
-        current_fs = $(this).parent();
-        previous_fs = $(this).parent().prev();
-
-        //Remove class active
-        $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-
-        //show the previous fieldset
-        previous_fs.show();
-
-        //hide the current fieldset with style
-        current_fs.animate({ opacity: 0 }, {
+    $(".previous").click(function ()
+    {
+        currentFs = $(this).parent();
+        var currentFs2 = $(this).parent().next();
+        previousFs = $(this).parent().prev();
+        
+        $("#progressbar li").eq($("fieldset").index(currentFs2)).removeClass("active");
+        
+        previousFs.show();
+        
+        currentFs.animate({ opacity: 0 }, {
             step: function (now) {
-                // for making fielset appear animation
                 opacity = 1 - now;
 
-                current_fs.css({
+                currentFs.css({
                     'display': 'none',
                     'position': 'relative'
                 });
-                previous_fs.css({ 'opacity': opacity });
+                previousFs.css({ 'opacity': opacity });
             },
             duration: 600
         });
@@ -59,8 +72,39 @@
         $(this).addClass('selected');
     });
 
-    $(".submit").click(function () {
+    $(".submit").click(function() {
         return false;
-    })
-
+    });
 });
+
+function validatePersonData() {
+    var fieldset1 = document.getElementById("fieldset1");
+    var inputItems = fieldset1.getElementsByTagName("input");
+    var itemsCount = inputItems.length;
+    var flags = [];
+
+    for (var i = 0; i < itemsCount; i++) {
+        if (inputItems[i].value === "") {
+            inputItems[i].style.borderColor = "red";
+            flags[i] = false;
+        }
+        else {
+            inputItems[i].style.borderColor = "lightgray";
+            flags[i] = true;
+        }
+    }
+
+    return allTrue(flags);
+}
+
+function validate2() {
+    return true;
+}
+
+function allTrue(obj) {
+    for (var k = 0; k < obj.length; k++) {
+        if (!obj[k]) {return false;}
+    }
+
+    return true;
+}
