a7c4859a2c09d909524ab5224a01a90212e7c86d
artur199209 <arti190@gmail.com>
2019-12-02 15:07:02 +0100

    remove required attribute

--- TheBookShop/Models/DataModels/DeliveryAddress.cs
+++ TheBookShop/Models/DataModels/DeliveryAddress.cs
@@ -7,19 +7,15 @@ namespace TheBookShop.Models.DataModels
     {
         [BindNever]
         public int DeliveryAddressId { get; set; }
-        [Required(ErrorMessage = "Proszę podać kraj.")]
         [Display(Name = "Kraj")]
         public string Country { get; set; }
-
-        [Required(ErrorMessage = "Proszę podać miasto.")]
+        
         [Display(Name = "Miasto")]
         public string City { get; set; }
-
-        [Required(ErrorMessage = "Proszę podać kod pocztowy.")]
+        
         [Display(Name = "Kod pocztowy")]
         public string ZipCode { get; set; }
-
-        [Required(ErrorMessage = "Proszę podać numer domu/mieszkania.")]
+        
         [Display(Name = "Nr domu/mieszkania")]
         public string HomeNumber { get; set; }
     }
