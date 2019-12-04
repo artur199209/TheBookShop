18c7d040e3a25c65aa2d442151e00fb020d6a80a
artur199209 <arti190@gmail.com>
2019-12-04 10:01:42 +0100

    Add EditTagHelper to add Id and Name attributes

---
+++ TheBookShop/Infrastructure/EditTagHelper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TheBookShop.Infrastructure
+{
+    [HtmlTargetElement("edit", Attributes = "ram")]
+    public class EditTagHelper : TagHelper
+    {
+        //[HtmlAttributeName("asp-for")]
+        public ModelExpression AspFor { get; set; }
+        
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var model = AspFor.ModelExplorer.Metadata.ContainerMetadata.ModelType.Name;
+            var propName = AspFor.Metadata.Name;
+            var id = $"{model}_{propName}";
+            var name = $"{model}.{propName}";
+
+            output.Attributes.Add("name", name);
+            output.Attributes.Add("id", id);
+            output.TagName = "input";
+        }
+    }
+}
