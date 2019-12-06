07bd8d55159bf5e0797ea1bb5b6fb315b38f8d36
artur199209 <arti190@gmail.com>
2019-12-06 13:59:57 +0100

    Add property do PaymentMethod model

--- TheBookShop/Models/DataModels/PaymentMethod.cs
+++ TheBookShop/Models/DataModels/PaymentMethod.cs
@@ -5,5 +5,6 @@
         public int PaymentMethodId { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
     }
 }
