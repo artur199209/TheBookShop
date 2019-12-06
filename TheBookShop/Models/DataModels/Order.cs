6cd7b200186bd4263f81922a6a722516f98035ad
artur199209 <arti190@gmail.com>
2019-12-06 14:00:47 +0100

    Refactor Order model

--- TheBookShop/Models/DataModels/Order.cs
+++ TheBookShop/Models/DataModels/Order.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace TheBookShop.Models.DataModels
@@ -14,18 +10,15 @@ namespace TheBookShop.Models.DataModels
         public int OrderId { get; set; }
         [BindNever]
         public ICollection<CartLine> Lines { get; set; }
-        
+        public string TrackingNumber { get; set; }
         public Customer Customer { get; set; }
-      
         public bool Shipped { get; set; }
-        
         public DeliveryAddress DeliveryAddress { get; set; }
         public Payment Payment { get; set; }
         public DeliveryMethod DeliveryMethod { get; set; }
         public PaymentMethod PaymentMethod { get; set; }
         public bool GiftWrap { get; set; }
-
-
+        
         public decimal CalculateTotalCosts()
         {
             var cartCost = Lines.Sum(x => x.Quantity * x.Product.Price);
