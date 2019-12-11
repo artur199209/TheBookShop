179467fdbfe3cbb339776b2bef0a048c991aea69
artur199209 <arti190@gmail.com>
2019-12-11 11:32:22 +0100

    Add DeliveryPaymentMethod

---
+++ TheBookShop/Models/DataModels/DeliveryPaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheBookShop.Models.DataModels
+{
+    public class DeliveryPaymentMethod
+    {
+        [ForeignKey(nameof(DeliveryMethodId))]
+        public int DeliveryMethodId { get; set; }
+        [ForeignKey(nameof(PaymentMethodId))]
+        public int PaymentMethodId { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public PaymentMethod PaymentMethod { get; set; }
+    }
+}
