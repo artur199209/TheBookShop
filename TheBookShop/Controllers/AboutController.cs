7d5c8723922600b1d9bf75c1cd27c5ef0f693827
artur199209 <arti190@gmail.com>
2020-01-02 13:56:04 +0100

    Add controller and views for footer info column

---
+++ TheBookShop/Controllers/AboutController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBookShop.Controllers
+{
+    [Route("[controller]")]
+    public class AboutController : Controller
+    {
+        [Route("")]
+        [Route("[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public ViewResult DeliveryMethods()
+        {
+            return View();
+        }
+
+        [Route("[action]")]
+        public ViewResult PaymentMethods()
+        {
+            return View();
+        }
+    }
+}
