7a4ab61eef69339b41f8d035045a5833cfd92747
artur199209 <arti190@gmail.com>
2020-01-02 13:57:19 +0100

    Add change password function

--- TheBookShop/Controllers/UserController.cs
+++ TheBookShop/Controllers/UserController.cs
@@ -12,11 +12,18 @@ namespace TheBookShop.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
+        
+        private readonly IPasswordValidator<AppUser> _passwordValidator;
+        private readonly IPasswordHasher<AppUser> _passwordHasher;
 
-        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+
+        public UserController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager,
+            IPasswordValidator<AppUser> passwordValidator, IPasswordHasher<AppUser> passwordHasher)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _passwordValidator = passwordValidator;
+            _passwordHasher = passwordHasher;
         }
 
         [Route("")]
@@ -61,6 +68,73 @@ namespace TheBookShop.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction(nameof(Login));
+        }
+
+        [Route("[action]")]
+        public async Task<IActionResult> ChangePassword(string id)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                return View(user);
+            }
+
+            return RedirectToAction("List", "Product", new { area = "" });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("[action]")]
+        public async Task<IActionResult> ChangePassword(string id, string email, string password)
+        {
+            AppUser user = await _userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                IdentityResult validPassword = null;
+
+                if (!string.IsNullOrEmpty(password))
+                {
+                    validPassword = await _passwordValidator.ValidateAsync(_userManager, user, password);
+
+                    if (validPassword.Succeeded)
+                    {
+                        user.PasswordHash = _passwordHasher.HashPassword(user, password);
+                    }
+                    else
+                    {
+                        AddErrorsFromResult(validPassword);
+                    }
+                }
+
+                if (validPassword == null ||
+                    password != string.Empty && validPassword.Succeeded)
+                {
+                    IdentityResult result = await _userManager.UpdateAsync(user);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("List", "Product", new {area = ""});
+                    }
+
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Nie znaleziono użytkownika.");
+            }
+
+            return View(user);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
         }
     }
 }
