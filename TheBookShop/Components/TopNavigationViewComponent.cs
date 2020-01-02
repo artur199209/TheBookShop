a6a7e9f81a2003ecfe7bdf6e85ec4deb08c7d121
artur199209 <arti190@gmail.com>
2020-01-02 09:22:29 +0100

    Update Top navigation panel

--- TheBookShop/Components/TopNavigationViewComponent.cs
+++ TheBookShop/Components/TopNavigationViewComponent.cs
@@ -29,29 +29,29 @@ namespace TheBookShop.Components
         public IEnumerable<NavbarItem> NavbarTop()
         {
             var topNav = new List<NavbarItem>();
-            topNav.Add(new NavbarItem() { Id = 1, Action = "Index", NameOption = "Programowanie", Controller = "Help", IsParent = false, ParentId = -1 });
-            topNav.Add(new NavbarItem() { Id = 2, Action = "Contact", NameOption = "Gry", Controller = "Home", IsParent = false, ParentId = -1 });
+            //topNav.Add(new NavbarItem() { Id = 1, Action = "Index", NameOption = "Programowanie", Controller = "Help", IsParent = false, ParentId = -1 });
+            //topNav.Add(new NavbarItem() { Id = 2, Action = "Contact", NameOption = "Gry", Controller = "Home", IsParent = false, ParentId = -1 });
             // drop down Menu 
-            topNav.Add(new NavbarItem() { Id = 3, Action = "Index", NameOption = "Programowanie", Controller = "Help", IsParent = true, ParentId = -1 });
-            topNav.Add(new NavbarItem() { Id = 4, Action = "Index", NameOption = ".NET/C#", Controller = "Cart", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 5, Action = "Index", NameOption = "Agile", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 6, Action = "Index", NameOption = "Algorytmy", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 7, Action = "Index", NameOption = "C/C++", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 8, Action = "Index", NameOption = "Delphi", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 9, Action = "Index", NameOption = "Go", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 10, Action = "Index", NameOption = "J2EE", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 11, Action = "Index", NameOption = "Java", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 12, Action = "Index", NameOption = "Objective-C/Swift", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 13, Action = "Index", NameOption = "Paradygmaty", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 14, Action = "Index", NameOption = "Perl", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 15, Action = "Index", NameOption = "Python", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 16, Action = "Index", NameOption = "Scala", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 17, Action = "Index", NameOption = "Visual Basic", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 18, Action = "Index", NameOption = "Visual Studio", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 19, Action = "Index", NameOption = "XAML", Controller = "Help", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 20, Action = "Index", NameOption = "Inne", Controller = "Help", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 3, Action = "Index", NameOption = "Programowanie", Controller = "Product", IsParent = true, ParentId = -1 });
+            topNav.Add(new NavbarItem() { Id = 4, Action = "List", NameOption = ".NET/C#", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 5, Action = "List", NameOption = "Agile", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 6, Action = "List", NameOption = "Algorytmy", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 7, Action = "List", NameOption = "C/C++", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 8, Action = "List", NameOption = "Delphi", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 9, Action = "List", NameOption = "Go", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 10, Action = "List", NameOption = "J2EE", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 11, Action = "List", NameOption = "Java", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 12, Action = "List", NameOption = "Objective-C/Swift", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 13, Action = "List", NameOption = "Paradygmaty", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 14, Action = "List", NameOption = "Perl", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 15, Action = "List", NameOption = "Python", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 16, Action = "List", NameOption = "Scala", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 17, Action = "List", NameOption = "Visual Basic", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 18, Action = "List", NameOption = "Visual Studio", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 19, Action = "List", NameOption = "XAML", Controller = "Product", IsParent = false, ParentId = 3 });
+            topNav.Add(new NavbarItem() { Id = 20, Action = "List", NameOption = "Inne", Controller = "Product", IsParent = false, ParentId = 3 });
             // End drop down Menu
-            topNav.Add(new NavbarItem() { Id = 21, Action = "Action", NameOption = "Bazy danych", Controller = "Home", IsParent = false, ParentId = -1 });
+            topNav.Add(new NavbarItem() { Id = 21, Action = "List", NameOption = "Bazy danych", Controller = "Product", IsParent = false, ParentId = -1 });
 
             topNav.Add(new NavbarItem() { Id = 22, Action = "Action", NameOption = "Nowości", Controller = "Home", IsParent = false, ParentId = -1 });
             topNav.Add(new NavbarItem() { Id = 23, Action = "Action", NameOption = "Zapowiedzi", Controller = "Home", IsParent = false, ParentId = -1 });
