f77475b33695127f9a61c477f38f7da57027d9df
artur199209 <arti190@gmail.com>
2020-01-17 14:38:56 +0100

    Added test for EnumExtensions class

---
+++ TheBookShop.Tests/InfrastructureTests/EnumExtensionsTests.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using TheBookShop.Infrastructure;
+using Xunit;
+
+namespace TheBookShop.Tests.InfrastructureTests
+{
+    public class EnumExtensionsTests
+    {
+        public enum EnumTest
+        {
+            [Description("First enum")]
+            New = 1,
+            [Description("Second enum")]
+            Old = 2
+        }
+
+        [Fact]
+        public void GetDescription_Returns_Enum_Description_Correctly()
+        {
+            var item1 = EnumTest.New.GetDescription();
+            var item2 = EnumTest.Old.GetDescription();
+
+            Assert.Equal("First enum", item1);
+            Assert.Equal("Second enum", item2);
+        }
+    }
+}
