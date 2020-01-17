f3b0c895c213a60fdb4559f24bcf59b660ed2494
artur199209 <arti190@gmail.com>
2020-01-17 14:23:57 +0100

    Added order status field

--- TheBookShop/Models/DataModels/Order.cs
+++ TheBookShop/Models/DataModels/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace TheBookShop.Models.DataModels
@@ -14,12 +15,24 @@ namespace TheBookShop.Models.DataModels
         public Guid OrderGuidId { get; set; }
         public string TrackingNumber { get; set; }
         public Customer Customer { get; set; }
+        public OrderStatus Status { get; set; }
         public bool Shipped { get; set; }
         public DeliveryAddress DeliveryAddress { get; set; }
         public Payment Payment { get; set; }
         public DeliveryPaymentMethod DeliveryPaymentMethod { get; set; }
         public bool GiftWrap { get; set; }
-        
+
+        public enum OrderStatus
+        {
+            [Description("Nowe zamówienie")]
+            New = 1,
+            [Description("W przygotowaniu")]
+            InProgress = 2,
+            [Description("Gotowe do wysłania")]
+            PreparedForShip = 3,
+            [Description("Wysłane")]
+            Shipped = 4
+        }
         //public decimal CalculateTotalCosts()
         //{
         //    var cartCost = Lines.Sum(x => x.Quantity * x.Product.Price);
