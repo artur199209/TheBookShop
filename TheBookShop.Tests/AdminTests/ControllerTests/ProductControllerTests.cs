e59d5a07e00927164f78a7ab795d658bc328357b
artur199209 <arti190@gmail.com>
2020-02-10 12:48:52 +0100

    Add unit tests

--- TheBookShop.Tests/AdminTests/ControllerTests/ProductControllerTests.cs
+++ TheBookShop.Tests/AdminTests/ControllerTests/ProductControllerTests.cs
@@ -1,9 +1,11 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
 using TheBookShop.Areas.Admin.Controllers;
 using TheBookShop.Models;
+using TheBookShop.Models.DataModels;
 using TheBookShop.Models.Repositories;
 using TheBookShop.Models.ViewModels;
 using Xunit;
@@ -23,7 +25,15 @@ namespace TheBookShop.Tests.AdminTests.ControllerTests
 
             _productRepositoryMock.Setup(m => m.Products).Returns(new[]
             {
-                new Product { ProductId = 1, Title = "Product1" },
+                new Product
+                {
+                    ProductId = 1, Title = "Product1", Opinions = new List<Opinion>
+                    {
+                        new Opinion { Name = "Name1", OpinionDescription = "OpinionDescription", Rating = 5 },
+                        new Opinion { Name = "Name2", OpinionDescription = "OpinionDescription", Rating = 5 },
+                        new Opinion { Name = "Name3", OpinionDescription = "OpinionDescription", Rating = 5 },
+                    }
+                },
                 new Product { ProductId = 2, Title = "Product2" },
                 new Product { ProductId = 3, Title = "Product3" },
             }.AsQueryable());
@@ -102,6 +112,74 @@ namespace TheBookShop.Tests.AdminTests.ControllerTests
             controller.Delete(_product.ProductId);
 
             _productRepositoryMock.Verify(m => m.DeleteProduct(_product.ProductId));
+        }
+
+        [Fact]
+        public void Can_Paginate_Products()
+        {
+            ProductController controller = new ProductController(_productRepositoryMock.Object, _authorRepositoryMock.Object) { PageSize = 3 };
+
+            ProductsListViewModel result = GetViewModel<ProductsListViewModel>(controller.Index());
+
+            var products = result?.Products.ToArray();
+
+            Assert.True(products?.Length == 3);
+            Assert.Equal("Product1", products[0].Title);
+            Assert.Equal("Product2", products[1].Title);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_For_Products()
+        {
+            ProductController controller = new ProductController(_productRepositoryMock.Object, _authorRepositoryMock.Object) { PageSize = 3 };
+
+            ProductsListViewModel result = GetViewModel<ProductsListViewModel>(controller.Index());
+
+            PagingInfo pageInfo = result?.PagingInfo;
+
+            Assert.Equal(3, pageInfo?.ItemsPerPage);
+            Assert.Equal(3, pageInfo?.TotalItems);
+            Assert.Equal(1, pageInfo?.TotalPages);
+            Assert.Equal(1, pageInfo?.CurrentPage);
+        }
+
+        [Fact]
+        public void Can_Display_Opinions()
+        {
+            ProductController controller = new ProductController(_productRepositoryMock.Object, _authorRepositoryMock.Object) { PageSize = 3 };
+            OpinionsListViewModel result = GetViewModel<OpinionsListViewModel>(controller.Opinion(1));
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.Opinions);
+            Assert.Equal(3, result.Opinions.Count());
+        }
+
+        [Fact]
+        public void Can_Paginate_Opinions()
+        {
+            ProductController controller = new ProductController(_productRepositoryMock.Object, _authorRepositoryMock.Object) { PageSize = 3 };
+            OpinionsListViewModel result = GetViewModel<OpinionsListViewModel>(controller.Opinion(1));
+
+            var opinions = result?.Opinions.ToArray();
+
+            Assert.True(opinions?.Length == 3);
+            Assert.Equal("Name1", opinions[0].Name);
+            Assert.Equal("Name2", opinions[1].Name);
+        }
+
+        [Fact]
+        public void Can_Send_Pagination_For_Opinions()
+        {
+            ProductController controller = new ProductController(_productRepositoryMock.Object, _authorRepositoryMock.Object) { PageSize = 3 };
+
+            OpinionsListViewModel result = GetViewModel<OpinionsListViewModel>(controller.Opinion(1));
+
+            PagingInfo pageInfo = result?.PagingInfo;
+
+            Assert.Equal(3, pageInfo?.ItemsPerPage);
+            Assert.Equal(3, pageInfo?.TotalItems);
+            Assert.Equal(1, pageInfo?.TotalPages);
+            Assert.Equal(1, pageInfo?.CurrentPage);
         }
 
         private T GetViewModel<T>(IActionResult result) where T : class
