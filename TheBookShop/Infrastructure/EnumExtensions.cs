6106cf760d598c4ec77e573d94b27d5b2f56af75
artur199209 <arti190@gmail.com>
2020-02-10 14:19:04 +0100

    Add method to get all description for enums

--- TheBookShop/Infrastructure/EnumExtensions.cs
+++ TheBookShop/Infrastructure/EnumExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
@@ -7,7 +9,7 @@ namespace TheBookShop.Infrastructure
 {
     public static class EnumExtensions
     {
-        public static string GetDescription(this Enum value)
+        public static string GetDescription(this object value)
         {
             return
                 value
@@ -17,6 +19,17 @@ namespace TheBookShop.Infrastructure
                     ?.GetCustomAttribute<DescriptionAttribute>()
                     ?.Description
                 ?? value.ToString();
+        }
+
+        public static IEnumerable<SelectListItem> GetEnumSelectList<T>()
+        {
+            return (Enum
+                .GetNames(typeof(T))
+                .Select(i => new SelectListItem
+                {
+                    Text = GetDescription(Enum.Parse(typeof(T), i.ToString())),
+                    Value = Enum.Parse(typeof(T), i.ToString()).ToString()
+                }));
         }
     }
 }
