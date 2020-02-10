d23194d3337182724660ed4d0b434967510145a5
artur199209 <arti190@gmail.com>
2020-02-10 14:19:15 +0100

    Add unit test

--- TheBookShop.Tests/InfrastructureTests/EnumExtensionsTests.cs
+++ TheBookShop.Tests/InfrastructureTests/EnumExtensionsTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Linq;
 using TheBookShop.Infrastructure;
 using Xunit;
 
@@ -11,7 +12,9 @@ namespace TheBookShop.Tests.InfrastructureTests
             [Description("First enum")]
             New = 1,
             [Description("Second enum")]
-            Old = 2
+            Old = 2,
+            [Description("In progress enum")]
+            InProgress = 3
         }
 
         [Fact]
@@ -19,9 +22,22 @@ namespace TheBookShop.Tests.InfrastructureTests
         {
             var item1 = EnumTest.New.GetDescription();
             var item2 = EnumTest.Old.GetDescription();
+            var item3 = EnumTest.InProgress.GetDescription();
 
             Assert.Equal("First enum", item1);
             Assert.Equal("Second enum", item2);
+            Assert.Equal("In progress enum", item3);
+        }
+
+        [Fact]
+        public void GetEnumSelectList_Returns_All_Descriptions()
+        {
+            var result = EnumExtensions.GetEnumSelectList<EnumTest>().ToArray();
+
+            Assert.NotNull(result);
+            Assert.Equal("First enum", result[0].Text);
+            Assert.Equal("Second enum", result[1].Text);
+            Assert.Equal("In progress enum", result[2].Text);
         }
     }
 }
