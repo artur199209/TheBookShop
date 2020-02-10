8378efef5c40b58f701607b96157822af65513d3
artur199209 <arti190@gmail.com>
2020-02-10 10:54:32 +0100

    Add view model for opinions

---
+++ TheBookShop/Models/ViewModels/OpinionsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TheBookShop.Models.DataModels;
+
+namespace TheBookShop.Models.ViewModels
+{
+    public class OpinionsListViewModel
+    {
+        public IEnumerable<Opinion> Opinions { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        public int ProductId { get; set; }
+    }
+}
