d90829dad892c4af8e6d3ceafec3380866cd159b
artur199209 <arti190@gmail.com>
2020-02-21 11:25:23 +0100

    Changed topnavigation to use data from DB + changes in view + unti tests

--- TheBookShop.Tests/ComponentTests/TopNavigationViewComponentTests.cs
+++ TheBookShop.Tests/ComponentTests/TopNavigationViewComponentTests.cs
@@ -1,29 +1,46 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
 using TheBookShop.Components;
+using TheBookShop.Models.DataModels;
+using TheBookShop.Models.Repositories;
 using Xunit;
 
 namespace TheBookShop.Tests.ComponentTests
 {
     public class TopNavigationViewComponentTests
     {
+        private readonly TopNavigationViewComponent _topNavigationViewComponent;
+
+        public TopNavigationViewComponentTests()
+        {
+            var productCategoryRepository = new Mock<IProductCategoryRepository>();
+
+            productCategoryRepository.Setup(m => m.ProductCategories).Returns(new[]
+            {
+                new ProductCategory { ProductCategoryId = 1 , Name = "Category1" },
+                new ProductCategory { ProductCategoryId = 2 , Name = "Category2" },
+                new ProductCategory { ProductCategoryId = 3 , Name = "Category3" },
+            }.AsQueryable());
+
+            _topNavigationViewComponent = new TopNavigationViewComponent(productCategoryRepository.Object);
+        }
+
         [Fact]
         public void Top_Navigation_Panel_Is_Not_Null()
         {
-            var target = new TopNavigationViewComponent();
-            var results = GetViewModel<IEnumerable<NavbarItem>>(target.Invoke()).ToArray();
+            var results = GetViewModel<IEnumerable<NavbarItem>>(_topNavigationViewComponent.Invoke()).ToArray();
 
-            Assert.NotNull(target);
+            Assert.NotNull(_topNavigationViewComponent);
             Assert.NotNull(results);
         }
 
         [Fact]
         public void Top_Navigation_Panel_Contains_Parents()
         {
-            var target = new TopNavigationViewComponent();
-            var results = GetViewModel<IEnumerable<NavbarItem>>(target.Invoke()).ToArray();
+            var results = GetViewModel<IEnumerable<NavbarItem>>(_topNavigationViewComponent.Invoke()).ToArray();
             var parents = results.Where(x => x.IsParent);
 
             Assert.NotNull(parents);
@@ -32,8 +49,7 @@ namespace TheBookShop.Tests.ComponentTests
         [Fact]
         public void Top_Navigation_Panel_Contains_Childrens()
         {
-            var target = new TopNavigationViewComponent();
-            var results = GetViewModel<IEnumerable<NavbarItem>>(target.Invoke()).ToArray();
+            var results = GetViewModel<IEnumerable<NavbarItem>>(_topNavigationViewComponent.Invoke()).ToArray();
             var childrens = results.Where(x => x.IsParent);
 
             Assert.NotNull(childrens);
--- TheBookShop/Components/TopNavigationViewComponent.cs
+++ TheBookShop/Components/TopNavigationViewComponent.cs
@@ -1,13 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using TheBookShop.Models.DataModels;
+using TheBookShop.Models.Repositories;
 
 namespace TheBookShop.Components
 {
     public class TopNavigationViewComponent : ViewComponent
     {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+
+        public TopNavigationViewComponent(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+
         public IViewComponentResult Invoke()
         {
-            Navbar navbar = new Navbar();
+            var navbar = new Navbar(_productCategoryRepository.ProductCategories);
 
             return View(navbar.NavbarTop());
         }
@@ -26,36 +36,36 @@ namespace TheBookShop.Components
     }
     public class Navbar
     {
+        private readonly IEnumerable<ProductCategory> _productCategories;
+
+        public Navbar(IEnumerable<ProductCategory> productCategories)
+        {
+            _productCategories = productCategories;
+        }
+
         public IEnumerable<NavbarItem> NavbarTop()
         {
-            var topNav = new List<NavbarItem>();
-            //topNav.Add(new NavbarItem() { Id = 1, Action = "Index", NameOption = "Programowanie", Controller = "Help", IsParent = false, ParentId = -1 });
-            //topNav.Add(new NavbarItem() { Id = 2, Action = "Contact", NameOption = "Gry", Controller = "Home", IsParent = false, ParentId = -1 });
-            // drop down Menu 
-            topNav.Add(new NavbarItem() { Id = 3, Action = "Index", NameOption = "Programowanie", Controller = "Product", IsParent = true, ParentId = -1 });
-            topNav.Add(new NavbarItem() { Id = 4, Action = "List", NameOption = ".NET/C#", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 5, Action = "List", NameOption = "Agile", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 6, Action = "List", NameOption = "Algorytmy", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 7, Action = "List", NameOption = "C/C++", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 8, Action = "List", NameOption = "Delphi", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 9, Action = "List", NameOption = "Go", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 10, Action = "List", NameOption = "J2EE", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 11, Action = "List", NameOption = "Java", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 12, Action = "List", NameOption = "Objective-C/Swift", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 13, Action = "List", NameOption = "Paradygmaty", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 14, Action = "List", NameOption = "Perl", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 15, Action = "List", NameOption = "Python", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 16, Action = "List", NameOption = "Scala", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 17, Action = "List", NameOption = "Visual Basic", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 18, Action = "List", NameOption = "Visual Studio", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 19, Action = "List", NameOption = "XAML", Controller = "Product", IsParent = false, ParentId = 3 });
-            topNav.Add(new NavbarItem() { Id = 20, Action = "List", NameOption = "Inne", Controller = "Product", IsParent = false, ParentId = 3 });
-            // End drop down Menu
-            topNav.Add(new NavbarItem() { Id = 21, Action = "List", NameOption = "Bazy danych", Controller = "Product", IsParent = false, ParentId = -1 });
+            var topNav = new List<NavbarItem>
+            {
+                new NavbarItem
+                {
+                    Id = 1,
+                    Action = "Index",
+                    NameOption = "Programowanie",
+                    Controller = "Product",
+                    IsParent = true,
+                    ParentId = -1
+                }
+            };
 
-            topNav.Add(new NavbarItem() { Id = 22, Action = "Action", NameOption = "Nowości", Controller = "Home", IsParent = false, ParentId = -1 });
-            topNav.Add(new NavbarItem() { Id = 23, Action = "Action", NameOption = "Zapowiedzi", Controller = "Home", IsParent = false, ParentId = -1 });
-            topNav.Add(new NavbarItem() { Id = 24, Action = "Action", NameOption = "Wyprzedaż", Controller = "Home", IsParent = false, ParentId = -1 });
+            foreach (var category in _productCategories)
+            {
+                topNav.Add(new NavbarItem { Id = topNav.Max(x => x.Id) + 1, Action = "List", NameOption = category.Name, Controller = "Product", IsParent = false, ParentId = 1 });
+            }
+            
+            topNav.Add(new NavbarItem { Id = topNav.Max(x => x.Id) + 1, Action = "Action", NameOption = "Nowości", Controller = "Home", IsParent = false, ParentId = -1 });
+            topNav.Add(new NavbarItem { Id = topNav.Max(x => x.Id) + 1, Action = "Action", NameOption = "Zapowiedzi", Controller = "Home", IsParent = false, ParentId = -1 });
+            topNav.Add(new NavbarItem { Id = topNav.Max(x => x.Id) + 1, Action = "Action", NameOption = "Wyprzedaż", Controller = "Home", IsParent = false, ParentId = -1 });
 
             return topNav;
         }
