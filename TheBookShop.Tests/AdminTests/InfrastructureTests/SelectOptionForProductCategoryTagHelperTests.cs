de4165661b733b5b1164332f699004dccc1e9fef
artur199209 <arti190@gmail.com>
2020-02-21 11:05:08 +0100

    Created Select option for product category + applied in view + unit tests

---
+++ TheBookShop.Tests/AdminTests/InfrastructureTests/SelectOptionForProductCategoryTagHelperTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheBookShop.Areas.Admin.Infrastructure;
+using TheBookShop.Models.DataModels;
+using TheBookShop.Models.Repositories;
+using Xunit;
+
+namespace TheBookShop.Tests.AdminTests.InfrastructureTests
+{
+    public class SelectOptionForProductCategoryTagHelperTests
+    {
+        private readonly Mock<IProductCategoryRepository> _productCategoryRepositoryMock;
+        private readonly TagHelperOutput _output;
+        private readonly TagHelperContext _ctx;
+
+        public SelectOptionForProductCategoryTagHelperTests()
+        {
+            _productCategoryRepositoryMock = new Mock<IProductCategoryRepository>();
+
+            var mockContent = new Mock<TagHelperContent>();
+
+            _ctx = new TagHelperContext(
+                new TagHelperAttributeList(),
+                new Dictionary<object, object>(), "");
+
+            _output = new TagHelperOutput("select",
+                new TagHelperAttributeList(),
+                (cache, encoder) => Task.FromResult(mockContent.Object));
+        }
+
+        [Fact]
+        public async Task Output_Is_Null_When_There_Are_No_Product_Categories()
+        {
+            _productCategoryRepositoryMock.Setup(x => x.ProductCategories).Returns(new List<ProductCategory>().AsQueryable);
+
+            var selectOptionForProductCategoryTagHelper = new SelectOptionForProductCategoryTagHelper(_productCategoryRepositoryMock.Object);
+            await selectOptionForProductCategoryTagHelper.ProcessAsync(_ctx, _output);
+
+            Assert.Equal("<option disabled selected>Wybierz kategorię</option>", _output.Content.GetContent());
+        }
+
+        [Fact]
+        public async Task Output_Contains_Product_Categories()
+        {
+            _productCategoryRepositoryMock.Setup(x => x.ProductCategories).Returns(new[]
+            {
+                new ProductCategory { ProductCategoryId = 1, Name = "Name1" },
+                new ProductCategory { ProductCategoryId = 2, Name = "Name2" }
+            }.AsQueryable());
+
+            var selectOptionForProductCategoryTagHelper = new SelectOptionForProductCategoryTagHelper(_productCategoryRepositoryMock.Object);
+            await selectOptionForProductCategoryTagHelper.ProcessAsync(_ctx, _output);
+
+            Assert.Equal("<option disabled selected>Wybierz kategorię</option><option value=1>Name1 </option><option value=2>Name2 </option>", _output.Content.GetContent());
+        }
+    }
+}
---
+++ TheBookShop/Areas/Admin/Infrastructure/SelectOptionForProductCategoryTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+using TheBookShop.Models.DataModels;
+using TheBookShop.Models.Repositories;
+
+namespace TheBookShop.Areas.Admin.Infrastructure
+{
+    [HtmlTargetElement("select", Attributes = "asp-for, ProductCategory")]
+    public class SelectOptionForProductCategoryTagHelper : TagHelper
+    {
+        private readonly IProductCategoryRepository _productCategoryRepository;
+
+        public SelectOptionForProductCategoryTagHelper(IProductCategoryRepository productCategoryRepository)
+        {
+            _productCategoryRepository = productCategoryRepository;
+        }
+
+        public ModelExpression ModelFor { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Content.AppendHtml((await output.GetChildContentAsync(false)).GetContent());
+
+            var selected = ModelFor?.Model as ProductCategory;
+
+            if (selected == null)
+            {
+                output.Content.AppendHtml($"<option disabled selected>Wybierz kategorię</option>");
+            }
+
+            foreach (var productCategory in _productCategoryRepository.ProductCategories)
+            {
+                output.Content.AppendHtml(selected?.ProductCategoryId != productCategory.ProductCategoryId
+                    ? $"<option value={productCategory.ProductCategoryId}>{productCategory.Name} </option>"
+                    : $"<option selected value={productCategory.ProductCategoryId}>{productCategory.Name} </option>");
+            }
+
+            output.Attributes.SetAttribute("Name", ModelFor?.Name);
+            output.Attributes.SetAttribute("Id", ModelFor?.Name);
+        }
+    }
+}
