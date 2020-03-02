91dc92068947753b9c47beb404c3f31110028222
artur199209 <arti190@gmail.com>
2020-03-02 12:49:48 +0100

    Add new unit test

--- TheBookShop.Tests/ControllerTests/ProductControllerTests.cs
+++ TheBookShop.Tests/ControllerTests/ProductControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 using TheBookShop.Controllers;
 using TheBookShop.Models.DataModels;
@@ -18,41 +19,66 @@ namespace TheBookShop.Tests.ControllerTests
 
             _productRepositoryMock.Setup(m => m.Products).Returns(new []
             {
-                new Product {ProductId = 1, Title = "Product1"},
-                new Product {ProductId = 2, Title = "Product2"},
-                new Product {ProductId = 3, Title = "Product3"},
-                new Product {ProductId = 4, Title = "Product4"},
-                new Product {ProductId = 5, Title = "Product5"}
+                new Product { ProductId = 1, Title = "Product1", IsProductInPromotion = true },
+                new Product { ProductId = 2, Title = "Product2", IsProductInPromotion = true },
+                new Product { ProductId = 3, Title = "Product3", IsProductInPromotion = true },
+                new Product { ProductId = 4, Title = "Product4", IsProductInPromotion = true },
+                new Product { ProductId = 5, Title = "Product5", IsProductInPromotion = true },
+                new Product { ProductId = 6, Title = "Product6", IsProductInPromotion = false },
+                new Product { ProductId = 7, Title = "Product7", IsProductInPromotion = false },
+                new Product { ProductId = 8, Title = "Product8", IsProductInPromotion = false },
+                new Product { ProductId = 9, Title = "Product9", IsProductInPromotion = false },
             }.AsQueryable());
         }
 
         [Fact]
         public void Can_Paginate()
         {
-            ProductController controller = new ProductController(_productRepositoryMock.Object) { PageSize = 3 };
+            var controller = new ProductController(_productRepositoryMock.Object) { PageSize = 3 };
 
-            ProductsListViewModel result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
+            var result = GetViewModel<ProductsListViewModel>(controller.List(null, 2));
 
             var products = result?.Products.ToArray();
             
-            Assert.True(products?.Length == 2);
+            Assert.True(products?.Length == 3);
             Assert.Equal("Product4", products[0].Title);
             Assert.Equal("Product5", products[1].Title);
+            Assert.Equal("Product6", products[2].Title);
         }
 
         [Fact]
         public void Can_Send_Pagination()
         {
-            ProductController controller = new ProductController(_productRepositoryMock.Object) { PageSize = 3 };
+            var controller = new ProductController(_productRepositoryMock.Object) { PageSize = 3 };
 
-            ProductsListViewModel result = controller.List(null, 2).ViewData.Model as ProductsListViewModel;
+            var result = GetViewModel<ProductsListViewModel>(controller.List(null, 2));
 
-            PagingInfo pageInfo = result?.PagingInfo;
+            var pageInfo = result?.PagingInfo;
 
             Assert.Equal(3, pageInfo?.ItemsPerPage);
-            Assert.Equal(5, pageInfo?.TotalItems);
-            Assert.Equal(2, pageInfo?.TotalPages);
+            Assert.Equal(9, pageInfo?.TotalItems);
+            Assert.Equal(3, pageInfo?.TotalPages);
             Assert.Equal(2, pageInfo?.CurrentPage);
+        }
+
+        [Fact]
+        public void Index_Contains_Data_For_Carousel()
+        {
+            var productController = new ProductController(_productRepositoryMock.Object) { NewProductsCount = 3 };
+
+            var result = GetViewModel<CarouselViewModel>(productController.Index());
+
+            var newProducts = result.NewProducts.ToList();
+            var promotionProducts = result.ProductsInThePromotion.ToList();
+
+            Assert.NotNull(result);
+            Assert.Equal(5, promotionProducts.Count);
+            Assert.Equal(3, newProducts.Count);
+        }
+
+        private T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
         }
     }
 }
