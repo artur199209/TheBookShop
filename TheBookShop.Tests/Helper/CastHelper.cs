2a168d2314bc49915171fddefd30022feb03fd7e
artur199209 <arti190@gmail.com>
2020-03-03 15:33:31 +0100

    Added helper for casting

---
+++ TheBookShop.Tests/Helper/CastHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheBookShop.Tests.Helper
+{
+    public static class CastHelper
+    {
+        private static T GetViewModel<T>(IActionResult result) where T : class
+        {
+            return (result as ViewResult)?.ViewData.Model as T;
+        }
+
+        private static string GetActionName(IActionResult result)
+        {
+            return (result as RedirectToActionResult)?.ActionName;
+        }
+    }
+}
