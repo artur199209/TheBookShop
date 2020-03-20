4c4f1dbf9fbd4fb5dccc1243c10735eac38cf16c
artur199209 <arti190@gmail.com>
2020-03-20 08:53:41 +0100

    Added model for charts

---
+++ TheBookShop/Models/DataPoint.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.Serialization;
+
+namespace TheBookShop.Models
+{
+    [DataContract]
+    public class DataPoint
+    {
+        public DataPoint(){}
+
+        public DataPoint(string label, double y)
+        {
+            Label = label;
+            Y = y;
+        }
+        
+        [DataMember(Name = "label")]
+        public string Label;
+        
+        [DataMember(Name = "y")]
+        public double? Y;
+    }
+}
