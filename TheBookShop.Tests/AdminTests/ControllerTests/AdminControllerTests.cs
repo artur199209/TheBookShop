a84cb5b5e2cd024b3b0a610ab8f3987b6075040e
artur199209 <arti190@gmail.com>
2020-03-20 11:55:55 +0100

    Added new unit test

--- TheBookShop.Tests/AdminTests/ControllerTests/AdminControllerTests.cs
+++ TheBookShop.Tests/AdminTests/ControllerTests/AdminControllerTests.cs
@@ -1,8 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Moq;
 using TheBookShop.Areas.Admin.Controllers;
+using TheBookShop.Models;
 using TheBookShop.Models.DataModels;
+using TheBookShop.Models.Repositories;
 using TheBookShop.Tests.Helper;
 using Xunit;
 using SignInResult = Microsoft.AspNetCore.Identity.SignInResult;
@@ -13,6 +17,7 @@ namespace TheBookShop.Tests.AdminTests.ControllerTests
     {
         private readonly Mock<UserManager<AppUser>> _userManagerMock;
         private readonly Mock<SignInManager<AppUser>> _signInManagerMock;
+        private readonly Mock<IOrderRepository> _orderRepositoryMock;
 
         public AdminControllerTests()
         {
@@ -21,6 +26,9 @@ namespace TheBookShop.Tests.AdminTests.ControllerTests
             var userPrincipalFactoryMock = new Mock<IUserClaimsPrincipalFactory<AppUser>>();
             _userManagerMock = new Mock<UserManager<AppUser>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
             _signInManagerMock = new Mock<SignInManager<AppUser>>(_userManagerMock.Object, contextAccessorMock.Object, userPrincipalFactoryMock.Object, null, null, null);
+            _orderRepositoryMock = new Mock<IOrderRepository>();
+
+           PrepareOrderRepositoryMock();
         }
 
         [Fact]
@@ -61,6 +69,93 @@ namespace TheBookShop.Tests.AdminTests.ControllerTests
             _signInManagerMock.Verify(m => m.PasswordSignInAsync(It.IsAny<AppUser>(), It.IsAny<string>(), false, false));
 
             Assert.Equal("Index", result);
+        }
+
+        [Fact]
+        public void Index_Contains_Data_For_Charts()
+        {
+            _userManagerMock.Setup(x => x.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new AppUser());
+            _signInManagerMock.Setup(x => x.PasswordSignInAsync(It.IsAny<AppUser>(), It.IsAny<string>(), false, false))
+                .ReturnsAsync(SignInResult.Success);
+            var controller = new AdminController(_userManagerMock.Object, _signInManagerMock.Object, _orderRepositoryMock.Object);
+
+            var chartsData = CastHelper.GetViewModel<List<List<DataPoint>>>(controller.Index());
+            
+            Assert.NotNull(chartsData);
+            Assert.Equal(2, chartsData.Count);
+        }
+
+        private void PrepareOrderRepositoryMock()
+        {
+            var category1 = new ProductCategory()
+            {
+                ProductCategoryId = 1,
+                Name = "Category1"
+            };
+
+            var category2 = new ProductCategory()
+            {
+                ProductCategoryId = 2,
+                Name = "Category2"
+            };
+
+            var product1 = new Product
+            {
+                ProductId = 1,
+                Title = "Title1",
+                Category = category1
+            };
+
+            var product2 = new Product
+            {
+                ProductId = 2,
+                Title = "Title2",
+                Category = category2
+            };
+
+            _orderRepositoryMock.Setup(x => x.Orders).Returns(new[]
+            {
+                new Order
+                {
+                    OrderId = 1, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Status = Order.OrderStatus.Shipped, Lines = new List<CartLine>()
+                    {
+                        new CartLine
+                        {
+                            CartLineId = 1, Product = product1, Quantity = 1
+                        },
+                        new CartLine
+                        {
+                            CartLineId = 2, Product = product2, Quantity = 2
+                        }
+                    }
+                },
+                new Order { OrderId = 2, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Status = Order.OrderStatus.InProgress },
+                new Order { OrderId = 3, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Status = Order.OrderStatus.InProgress, },
+                new Order { OrderId = 4, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Status = Order.OrderStatus.Shipped, Lines = new List<CartLine>()
+                    {
+                        new CartLine
+                        {
+                            CartLineId = 1, Product = product1, Quantity = 2
+                        },
+                        new CartLine
+                        {
+                            CartLineId = 2, Product = product2, Quantity = 1
+                        }
+                    }
+                },
+                new Order { OrderId = 5, Customer = new Customer(), DeliveryAddress =  new DeliveryAddress(), Status = Order.OrderStatus.Shipped, Lines = new List<CartLine>()
+                    {
+                        new CartLine
+                        {
+                            CartLineId = 1, Product = product1, Quantity = 1
+                        },
+                        new CartLine
+                        {
+                            CartLineId = 2, Product = product2, Quantity = 2
+                        }
+                    }
+                }
+            }.AsQueryable());
         }
     }
 }
